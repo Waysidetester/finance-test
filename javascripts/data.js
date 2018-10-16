6d0375b37861ddb5fb76edb6bcf68b07e675e3d8
WaysideTester <dylanmurray2119@gmail.com>
2018-10-16 18:10:37 -0500

    wip

--- javascripts/data.js
+++ javascripts/data.js
@@ -6,9 +6,21 @@ let ticker = "";
 $("#ticker").on("keyup", (e) => {
 if (e.keyCode === 13) {
     ticker = $("#ticker").val();
-    $.get(`${baseUrl}/stock/${ticker}/batch?types=quote`)
+    $.get(`${baseUrl}/stock/${ticker}/company`)
     .done((data) => {
-        console.log(data.quote.close);
+        console.log(data);
+        let newString = "";
+        newString += `<p>${data.symbol}</p>`;
+        newString += `<p>${data.CEO}</p>`;
+        newString += `<p>${data.companyName}</p>`;
+        newString += `<p>${data.description}</p>`;
+        newString += `<p>${data.exchange}</p>`;
+        newString += `<p>${data.industry}</p>`;
+        newString += `<p>${data.issueType}</p>`;
+        newString += `<p>${data.sector}</p>`;
+        newString += `<p>${data.tags}</p>`;
+        newString += `<p>${data.website}</p>`;
+        $("#printHere").html(`<p>${newString}</p>`);
     })
     .fail((error) => {
         console.error("error");
