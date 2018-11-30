812b31f9ec978e2e7759b5159b86715f51b808b4
WaysideTester <dylanmurray211@outlook.com>
2018-11-29 19:09:37 -0600

    starting auth event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import firebase from 'firebase/app';
+import apiKeys from '../db/apiKeys.json';
 import search from './javascripts/components/search/search';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   search.searchBar();
 };
 
